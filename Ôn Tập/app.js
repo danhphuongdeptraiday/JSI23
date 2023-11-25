d65a3536c85df8da182b4a7af8b37a6fec292872
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-11-25 19:53:23 +0700

    upcode to JSI23

---
+++ Ôn Tập/app.js
@@ -0,0 +1,68 @@
+// let value_from_input = prompt("Enter something ? ");
+// console.log(value_from_input);
+
+// if (isNaN(value_from_input) == true) {
+//   let h2 = document.createElement("h2");
+//   h2.innerText = "Nhập không đúng dữ liệu đầu vào, vui lòng nhập lại";
+//   h2.style.color = "red";
+//   document.body.appendChild(h2);
+// } else {
+//   let convert_number = Number(value_from_input);
+
+//   if (convert_number == 10) {
+//     document.write(value_from_input);
+//     let h2 = document.createElement("h2");
+//     h2.innerText = "Điểm tuyệt đối";
+//     h2.style.color = "green";
+
+//     document.body.appendChild(h2);
+//   } else if (convert_number >= 0 && convert_number < 10) {
+//     document.write(value_from_input);
+//     let h2 = document.createElement("h2");
+//     h2.innerText = "Điểm chưa tuyệt đối";
+//     h2.style.color = "orange";
+
+//     document.body.appendChild(h2);
+//   } else if (convert_number < 0 || convert_number > 10) {
+//     document.write(value_from_input);
+//     let h2 = document.createElement("h2");
+//     h2.innerText = "Điểm bị nhập sai khoảng, vui lòng nhập lại";
+//     h2.style.color = "purple";
+
+//     document.body.appendChild(h2);
+//   }
+// }
+
+// let person = {
+//   name: "Phương",
+//   age: 100,
+//   list_favor: ["Sở thích 1", "Sở thích 2", "Sở thích 3"],
+//   eat: function () {
+//     console.log("Thích ăn cơm");
+//   },
+// };
+
+// // Tạo ra thẻ 3 thẻ li và mỗi thẻ li sẽ chứa 1 sở thích trong mảng list_favor. Dùng DOM tạo ra 3 thẻ li mới
+
+// console.log(person.list_favor[0]);
+
+// void function
+// let t = 10;
+
+// function do_something() {
+//   t = 20;
+//   console.log("Hello my fr");
+// }
+
+// do_something();
+// console.log(t);
+
+// Function khi có return
+
+// function enter_name(x) {
+//   return "Hello " + x;
+// }
+
+// enter_name(); // <=> "Hello My Friends"
+
+// console.log("Text = " + enter_name("Phương"));
